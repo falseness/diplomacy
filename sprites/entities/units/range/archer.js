67de04ca747ef0de16a271eb7a4005b604426648
falseness <baharevuniversal@gmail.com>
2019-07-20 18:35:55 +0300

    archer balance fix

--- sprites/entities/units/range/archer.js
+++ sprites/entities/units/range/archer.js
@@ -4,7 +4,7 @@ class Archer extends RangeUnit {
         const healSpeed = 1
         const dmg = 1
         const speed = 2
-        const salary = 2
+        const salary = 3
         const range = 3
         super(x, y, 'archer', hp, healSpeed, dmg, range, speed, salary, town)
     }
