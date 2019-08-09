065de81642c926df7a65123f177e740d7118333c
falseness <baharevuniversal@gmail.com>
2019-08-09 19:40:03 +0300

    normchel balance fix

--- sprites/entities/units/normchel.js
+++ sprites/entities/units/normchel.js
@@ -1,9 +1,9 @@
 class Normchel extends Unit {
-    static maxHP = 5
+    static maxHP = 6
     static healSpeed = 2
     static dmg = 2
     static speed = 2
-    static salary = 3
+    static salary = 4
     constructor(x, y) {
         super(x, y, 'normchel')
     }
