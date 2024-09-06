e56e3ff96d5aa3b17adbd7966395742ccf1a6e97
Falseness <baharevuniversal@gmail.com>
2024-09-06 22:51:37 +0300

    feat fix bug

--- options/gamestart.js
+++ options/gamestart.js
@@ -31,10 +31,11 @@ class Map {
     createGoldmines() {
         for (let i = 0; i < this.goldmines.length; ++i) {
             let roundsToOpen = 0
+            let goldmine = this.goldmines[i]
             if ('roundsToOpen' in goldmine) {
                 roundsToOpen = goldmine.roundsToOpen
             }
-            let goldmine = this.goldmines[i]
+            
             new Goldmine(goldmine.x, goldmine.y, goldmine.income, roundsToOpen)
         }
     }
